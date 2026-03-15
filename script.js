e7b9d66d6a50da084dfeb9b11b5d3025decef95e
john-marco-dimaano <john.dimaanos4@gmail.com>
2026-03-15 10:18:19 +0100

    change automatico pagina

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ const slides = document.querySelectorAll('.slide');
 const lastSlide = document.querySelector('.last-slide');
 
 let currentSlide = 0;
+let autoPlayInterval = 6000; // ⬅️ cambia qui i secondi (6000 = 6s)
+let timer;
 
+// Mostra slide
 function showSlide(index) {
   slides.forEach(slide => slide.classList.remove('active'));
   slides[index].classList.add('active');
+}
+
+// Avanza di 1 slide
+function nextSlide() {
+  if (currentSlide < slides.length - 1) {
+    currentSlide++;
+  } else {
+    currentSlide = 0; // ricomincia dopo la terza
+  }
+  showSlide(currentSlide);
+}
+
+// Avvia autoplay
+function startAutoPlay() {
+  timer = setInterval(nextSlide, autoPlayInterval);
+}
+
+// Reset autoplay quando l’utente clicca
+function resetAutoPlay() {
+  clearInterval(timer);
+  startAutoPlay();
 }
 
 /* CLICK OVUNQUE → AVANTI */
@@ -14,6 +38,7 @@ slides.forEach((slide, index) => {
     if (index < slides.length - 1) {
       currentSlide = index + 1;
       showSlide(currentSlide);
+      resetAutoPlay();
     }
   });
 });
@@ -22,4 +47,8 @@ slides.forEach((slide, index) => {
 lastSlide.addEventListener('click', () => {
   currentSlide = 0;
   showSlide(currentSlide);
+  resetAutoPlay();
 });
+
+// Avvia autoplay all'apertura
+startAutoPlay();
